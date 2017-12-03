fe06f9fa9ac744047ecbb210d77e59bf9f33980c
Nick Klauer <klauer@gmail.com>
2017-12-02 22:37:47 -0600

    add day2 stuff

---
+++ day2/checksum.go
@@ -0,0 +1,30 @@
+package day2
+
+const spreadsheet = `5 1 9 5
+7 5 3
+2 4 6 8`
+
+// DifferenceLargestSmallest will find the largest and smallest numbers in a string,
+// and apply a difference on them to find the largest difference value.
+func DifferenceLargestSmallest(line string) int {
+	// TODO: call parseLine for each, then find the smallest and largest value, then difference.
+	return 0
+}
+
+func parseLine(line string) []int {
+	var values []int
+
+	// TODO: write parser
+	return values
+}
+
+// ChecksumTestSpreadsheet performs the checksum for the example `spreadsheet` value above.
+func ChecksumTestSpreadsheet() int {
+	// TODO: do a checksum of the sum of each line in spreadsheet
+	return 0
+}
+
+// ChecksumInput will checksum the input.txt file.
+func ChecksumInput() int {
+	return 0
+}
---
+++ day2/checksum_test.go
@@ -0,0 +1,109 @@
+package day2
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestDifferenceLargestSmallest(t *testing.T) {
+	type args struct {
+		line string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"5 1 9 5",
+			args{"5 1 9 5"},
+			8,
+		},
+		{
+			"7 5 3",
+			args{"7 5 3"},
+			4,
+		},
+		{
+			"2 4 6 8",
+			args{"2 4 6 8"},
+			6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DifferenceLargestSmallest(tt.args.line); got != tt.want {
+				t.Errorf("DifferenceLargestSmallest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parseLine(t *testing.T) {
+	type args struct {
+		line string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			"7 5 3",
+			args{"7 5 3"},
+			[]int{7, 5, 3},
+		},
+		{
+			"2 4 6 8",
+			args{"2 4 6 8"},
+			[]int{2, 4, 6, 8},
+		},
+		{
+			"5 1 9 5",
+			args{"5 1 9 5"},
+			[]int{5, 1, 9, 5},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parseLine(tt.args.line); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseLine() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestChecksumTestSpreadsheet(t *testing.T) {
+	tests := []struct {
+		name string
+		want int
+	}{
+		{
+			"sample test",
+			18,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ChecksumTestSpreadsheet(); got != tt.want {
+				t.Errorf("ChecksumTestSpreadsheet() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestChecksumInput(t *testing.T) {
+	tests := []struct {
+		name string
+		want int
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ChecksumInput(); got != tt.want {
+				t.Errorf("ChecksumInput() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
