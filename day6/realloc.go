fc52a55951ffa3d15539fc3f286f1e8dae86873b
Nick Klauer <klauer@gmail.com>
2017-12-08 17:35:41 -0600

    stub out initial thing for day 6

---
+++ day6/realloc.go
@@ -0,0 +1,9 @@
+package day6
+
+// NumReallocsCycle will calculate part 1 of the chore, where we determine the
+// number of memory reallocations that are needed before we see our first
+// repetition.
+func NumReallocsCycle(mem []int) int {
+	return -1
+}
+
