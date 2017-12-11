24c3cca12d6e0fb30c17fc6d422b3e4c8ec01ddb
Nick Klauer <klauer@gmail.com>
2017-12-11 16:43:47 -0600

    day 8 stuff

---
+++ day8/register.go
@@ -0,0 +1,126 @@
+package day8
+
+import (
+	"strconv"
+	"strings"
+)
+
+type Operation int
+type Comparator int
+
+const (
+	INC Operation = iota
+	DEC
+	NONE
+	LT Comparator = iota
+	LTE
+	GT
+	GTE
+	EQU
+	NOT
+	NULL
+)
+
+type comparison struct {
+	register string
+	comp     Comparator
+	value    int
+}
+
+type instruction struct {
+	register   string
+	operation  Operation
+	amount     int
+	comparison comparison
+}
+
+type registers map[string]int
+
+func newComparator(comp string) Comparator {
+	switch comp {
+	case "<=":
+		return LTE
+	case "<":
+		return LT
+	case ">":
+		return GT
+	case ">=":
+		return GTE
+	case "==":
+		return EQU
+	case "!=":
+		return NOT
+	default:
+		return NULL
+	}
+}
+
+func newComparison(fields []string) comparison {
+	v, err := strconv.Atoi(fields[2])
+	if err != nil {
+		v = 0
+	}
+	return comparison{
+		register: fields[0],
+		comp:     newComparator(fields[1]),
+		value:    v,
+	}
+}
+
+func newOperator(op string) Operation {
+	switch op {
+	case "dec":
+		return DEC
+	case "inc":
+		return INC
+	default:
+		return NONE
+	}
+}
+
+func newInstruction(line string) instruction {
+	fields := strings.Fields(line)
+	amtBy, err := strconv.Atoi(fields[2])
+	if err != nil {
+		amtBy = 0
+	}
+	return instruction{
+		register:   fields[0],
+		operation:  newOperator(fields[1]),
+		amount:     amtBy,
+		comparison: newComparison(fields[4:7]),
+	}
+}
+
+func (r registers) execute(inst instruction) {
+	if r.evaluate(inst.comparison) {
+		switch inst.operation {
+		case DEC:
+			r[inst.register] -= inst.amount
+		case INC:
+			r[inst.register] += inst.amount
+		default:
+			break
+		}
+	}
+}
+
+func (r registers) evaluate(comp comparison) bool {
+	reg := r[comp.register]
+	switch comp.comp {
+	case LTE:
+		return reg <= comp.value
+	case LT:
+		return reg < comp.value
+	case GT:
+		return reg > comp.value
+	case GTE:
+		return reg >= comp.value
+	case EQU:
+		return reg == comp.value
+	case NOT:
+		return reg != comp.value
+	default:
+		return false
+	}
+}
---
+++ day8/register_test.go
@@ -0,0 +1,140 @@
+package day8
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_newComparator(t *testing.T) {
+	type args struct {
+		comp string
+	}
+	tests := []struct {
+		name string
+		args args
+		want Comparator
+	}{
+		{
+			"less than",
+			args{"<"},
+			LT,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := newComparator(tt.args.comp); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("newComparator() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_newComparison(t *testing.T) {
+	type args struct {
+		fields []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want comparison
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := newComparison(tt.args.fields); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("newComparison() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_newOperator(t *testing.T) {
+	type args struct {
+		op string
+	}
+	tests := []struct {
+		name string
+		args args
+		want Operation
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := newOperator(tt.args.op); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("newOperator() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_newInstruction(t *testing.T) {
+	type args struct {
+		line string
+	}
+	tests := []struct {
+		name string
+		args args
+		want instruction
+	}{
+		{
+			"b inc 5 if a > 1",
+			args{"b inc 5 if a > 1"},
+			instruction{
+				amount: 5,
+				comparison: comparison{
+					comp:     GT,
+					register: "a",
+					value:    1,
+				},
+				operation: INC,
+				register:  "b",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := newInstruction(tt.args.line); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("newInstruction() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_registers_execute(t *testing.T) {
+	type args struct {
+		inst instruction
+	}
+	tests := []struct {
+		name string
+		r    registers
+		args args
+	}{}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.r.execute(tt.args.inst)
+		})
+	}
+}
+
+func Test_registers_evaluate(t *testing.T) {
+	type args struct {
+		comp comparison
+	}
+	tests := []struct {
+		name string
+		r    registers
+		args args
+		want bool
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.r.evaluate(tt.args.comp); got != tt.want {
+				t.Errorf("registers.evaluate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
